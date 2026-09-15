5e78392edd87b27786ffc79df155bcb0f61a4a3f
user <pawel.m.janik@gmail.com>
2026-09-15 13:05:57 +0200

    Task completed. Created Dao and Service methods and implementations, created hibernate.cfg.xml, created DataProcessingException, created Movie model.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,17 @@
 package mate.academy;
 
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        MovieService movieService = new MovieServiceImpl();
+        Movie movie = new Movie();
+        movie.setTitle("Big trouble in little China");
+        movieService.add(movie);
 
+        Movie movieFromDb = movieService.get(1L);
+        System.out.println(movieFromDb);
     }
 }
